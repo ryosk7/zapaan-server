692e989f71b95344419ae981bc84efa7ebc0cf45
ryosk7 <uchiryo7@gmail.com>
2020-04-21 12:56:03 +0900

    feat(sheet): 初期設定を追加 start_time

--- zapaan/app/models/sheet.rb
+++ zapaan/app/models/sheet.rb
@@ -1,3 +1,11 @@
 class Sheet < ApplicationRecord
   acts_as_paranoid without_default_scope: true
+
+  before_create do |sheet|
+    sheet.default_settings
+  end
+
+  def default_settings
+    self.start_time = Date.today
+  end
 end
